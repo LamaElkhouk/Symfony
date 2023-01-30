9b01954b0d0e02f2fa19537b20f256d8d43b3fc2
LamaElkhouk <lamiaelkhoukhi09@gmail.com>
2023-01-30 16:59:59 +0100

    creation entité products

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,18 +5,58 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class TestController
+class TestController extends AbstractController
 {
+    private array $tab = [
+        'item1' => ["nom" => "item1", "prix" => 10, "image" => "images/robe_grise.jpg"],
+        'item2' => ["nom" => "item2", "prix" => 11, "image" => "images/robe_noire.jpg"],
+        'item3' => ["nom" => "item3", "prix" => 9, "image" => "images/robe_rose.jpg"],
+        'item4' => ["nom" => "item4", "prix" => 12, "image" => "images/robe_verte.jpg"],
+    ];
+
+
     /**
      * @Route("/",name="index")
      * @return void
      */
     public function index()
     {
-        var_dump("ça fonctionne");
-        die();
+        /*var_dump("ça fonctionne");
+        die();*/
+
+        $titre = "titre principal";
+
+
+        /*$tab =[
+            {'id':1;'nom':'item'}
+        ]*/
+        // $nb = count($this->tab);
+        return $this->render('default/index.html.twig', ['titre' => $titre, 'tab' => $this->tab,]);
     }
+    /**
+     * Undocumented function
+     *
+     * @param [type] $nom
+     * @return void
+     * @Route("/article/{nom}",name="detail")
+     */
+    public function detail($nom)
+    {
+        $titre = "detail d'un produit : ";
+
+        // tester si le produit existe dans la liste
+        if (array_key_exists($nom, $this->tab)) {
+            return $this->render('default/detail.html.twig', ['titre' => $titre, 'nom' => $nom]);
+        }
+    }
+    /**
+     * Undocumented function
+     *
+     * @param Request $request
+     * @return void
+     */
     public function test(Request $request)
     {
         // $request = Request::createFromGlobals();
